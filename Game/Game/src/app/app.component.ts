9e1472c1633ef5160b093b29e5719ac1eeb7476a
say25 <say25@cornell.edu>
2017-08-31 22:26:05 -0500

    Install Angular
    - Get Angular Up and Running

---
+++ Game/Game/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  constructor() { }
+  // constructor(private _httpService: Http) { }
+   apiValues: string[] = [];
+   title: string = 'DorkGame'
+    ngOnInit() {
+  //     this._httpService.get('/api/player').subscribe(values => {
+  //       this.apiValues = values.json() as string[];
+  //     });
+    }
+}
