1bc564e0cd8eac32112b59f68d8e781c42ff49f1
kassemsandarusi <=>
2017-09-07 19:14:16 -0500

    Adding gameplayer logic.

---
+++ Game/Game/Data/GamePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game.Data
+{
+	public class GamePlayer
+	{
+		public Guid Id { get; set; }
+		public Player Player { get; set; }
+		public Game Game { get; set; }
+		public int Score { get; set; }
+		public int Rank { get; set; }
+	}
+}
