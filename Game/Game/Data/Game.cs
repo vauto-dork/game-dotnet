98baedf0eaa75b531aa6ff873fba11087c7abd72
Kassem Sandarusi <kassemsandarusi@gmail.com>
2017-09-07 21:42:37 -0500

    Adding gameplayer logic. (#35)

--- Game/Game/Data/Game.cs
+++ Game/Game/Data/Game.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Game.Data
 {
-    public class Game
-    {
-        public Guid Id { get; set; }
-    }
+	public class Game
+	{
+		public Guid Id { get; set; }
+		public IList<GamePlayer> Players { get; set; }
+	}
 }
--- Game/Game/Data/GameContext.cs
+++ Game/Game/Data/GameContext.cs
@@ -2,17 +2,28 @@
 
 namespace Game.Data
 {
-    public class GameContext : DbContext
-    {
-        public DbSet<Player> Player { get; set; }
-        public DbSet<Game> Game { get; set; }
+	public class GameContext : DbContext
+	{
+		public DbSet<Player> Player { get; set; }
+		public DbSet<Game> Game { get; set; }
+		public DbSet<GamePlayer> GamePlayer { get; set; }
 
-        public GameContext(DbContextOptions options) : base(options) { }
+		public GameContext(DbContextOptions options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Player>()
-                .HasKey(data => data.Id);
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Game>()
+				.HasKey(game => game.Id);
+			modelBuilder.Entity<Player>()
+				.HasKey(player => player.Id);
+			modelBuilder.Entity<GamePlayer>()
+				.HasKey(gamePlayer => gamePlayer.Id);
+			modelBuilder.Entity<GamePlayer>()
+				.HasOne(gamePlayer => gamePlayer.Player)
+				.WithMany(player => player.History);
+			modelBuilder.Entity<GamePlayer>()
+				.HasOne(gamePlayer => gamePlayer.Game)
+				.WithMany(player => player.Players);
+		}
+	}
 }
